333ebdfecd7642465614070fe216e2976bfdf3d7
kadyr2601 <2601kadyr@gmail.com>
2025-03-26 08:24:36 +0100

    update config

--- next.config.ts
+++ next.config.ts
@@ -4,22 +4,22 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '8000',
-                pathname: '/media/**',
-
-                // protocol: 'https',
-                // hostname: 'nxt-fitout.com',
-                // port: '',
+                // protocol: 'http',
+                // hostname: '127.0.0.1',
+                // port: '8000',
                 // pathname: '/media/**',
+
+                protocol: 'https',
+                hostname: 'nxt-fitout.com',
+                port: '',
+                pathname: '/media/**',
             },
         ],
     },
     env: {
-        API_URL: "http://127.0.0.1:8000",
-        // HostName: "https://nxt-fitout.com",
-        // API_URL: "https://nxt-fitout.com",
+        // API_URL: "http://127.0.0.1:8000",
+        HostName: "https://nxt-fitout.com",
+        API_URL: "https://nxt-fitout.com",
     },
 };
 
